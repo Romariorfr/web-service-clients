37c62508dc481da0887d8fefa8ffaba4f297a3a1
Romário Ferreira <romarioferreirarezende@hotmail.com>
2021-12-02 01:26:37 -0300

    project created

---
+++ backend/src/main/java/com/devsuperior/DSclient/DSclientApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.DSclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DSclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DSclientApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/DSclient/DSclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.DSclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DSclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
